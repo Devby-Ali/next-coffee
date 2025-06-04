409d28f779e397f621a7eaa725f6b362b6cc9ac7
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-04 17:31:16 +0330

    Implementation fonts

--- src/components/ui/fonts.ts
+++ src/components/ui/fonts.ts
@@ -1,45 +1,47 @@
-import localFont from 'next/font/local';
+import localFont from 'next/font/local'
+
 
 export const dana = localFont({
   src: [
     {
-      path: '../../fonts/Dana/DanaFaNum-Regular.woff2',
+      path: '../../../public/fonts/Dana/DanaFaNum-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../fonts/Dana/DanaFaNum-Medium.woff2',
+      path: '../../../public/fonts/Dana/DanaFaNum-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../fonts/Dana/DanaFaNum-DemiBold.woff2',
+      path: '../../../public/fonts/Dana/DanaFaNum-DemiBold.woff2',
       weight: '700',
       style: 'normal',
     },
   ],
   variable: '--font-dana',
   display: 'swap',
-});
+})
+
 
 export const morabba = localFont({
   src: [
     {
-      path: '../../fonts/Morabba/Morabba-Light.woff2',
-      weight: '400',
+      path: '../../../public/fonts/Morabba/Morabba-Light.woff2',
+      weight: '300',
       style: 'normal',
     },
     {
-      path: '../../fonts/Morabba/Morabba-Medium.woff2',
+      path: '../../../public/fonts/Morabba/Morabba-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../fonts/Morabba/Morabba-Bold.woff2',
+      path: '../../../public/fonts/Morabba/Morabba-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
   ],
   variable: '--font-morabba',
   display: 'swap',
-});
+})
