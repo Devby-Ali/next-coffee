d3cec41623e4d1ff5d982aa5bd8def7c14b1b97a
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-06 21:33:22 +0330

    Refactor Index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Header from "@/components/common/Header";
+import Header from '@/components/common/Header';
+import { fetchHomePageData } from '@/lib/data';
 
-export default function Home() {
+export default async function Home () {
+  const data = await fetchHomePageData();
+  console.log(data)
   return (
     <main>
-    <Header/>
+      <Header />
     </main>
   );
-}
+};
+
+// export async function getStaticProps() {
+//   const servicesResponse = await fetch("http://localhost:4000/services");
+//   const servicesData = await servicesResponse.json();
+
+//   const menuResponse = await fetch("http://localhost:4000/menu");
+//   const menuData = await menuResponse.json();
+
+//   const commentsResponse = await fetch("http://localhost:4000/comments");
+//   const commentsData = await commentsResponse.json();
+
+//   return {
+//     props: {
+//       data: {
+//         services: servicesData,
+//         menu: menuData,
+//         comments: commentsData,
+//       },
+//     },
+//     revalidate: 60 * 60 * 12, // Second
+//   };
+// }
+
+// export default Home;
