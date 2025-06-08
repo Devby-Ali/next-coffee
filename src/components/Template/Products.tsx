8841f21eca41a375c52870bc8009faab51970411
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-08 21:56:36 +0330

    Change Product Component styles

--- src/components/Template/Products.tsx
+++ src/components/Template/Products.tsx
@@ -5,7 +5,7 @@ import SectionHeader from '../ui/SectionHeader';
 
 function Products() {
   return (
-    <section className="products pt-8 md:pt-24 lg:pt-44">
+    <section className="products pt-8 md:pt-24">
       <div className="container">
         {/* <!-- Section Head --> */}
         <SectionHeader
@@ -15,7 +15,7 @@ function Products() {
         />
 
         {/* <!-- Section Body --> */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3.5 md:gap-5">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-3.5 md:gap-5">
           <div className="p-2 md:p-5 bg-white dark:bg-zinc-700 shadow-normal rounded-2xl">
             <div className="relative mb-2 md:mb-5">
               <Image
@@ -26,16 +26,16 @@ function Products() {
                 loading="lazy"
                 alt="product-1"
               />
-              <span className="absolute top-1.5 right-1.5 block h-5 md:h-[30px] text-xs/[24px] md:text-base/[34px] font-DanaDemiBold bg-orange-300 text-white dark:text-zinc-700 px-2.5 md:px-3.5 rounded-full">
+              <span className="absolute top-1.5 right-1.5 block h-5 md:h-7.5 text-xs/[24px] md:text-base/[34px] font-dana font-semibold bg-orange-300 text-white dark:text-zinc-700 px-2.5 md:px-3.5 rounded-full">
                 12%
               </span>
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
               <div className="text-teal-600 dark:text-emerald-500">
-                <span className="font-DanaDemiBold text-base md:text-xl">154,000</span>
+                <span className="font-dana font-semibold text-base md:text-xl">154,000</span>
                 <span className="text-xs md:text-sm tracking-tighter">تومان</span>
               </div>
               <div className="offer">
@@ -45,7 +45,7 @@ function Products() {
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
@@ -81,18 +81,18 @@ function Products() {
                 alt="product-2"
               />
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
               <div className="text-teal-600 dark:text-emerald-500">
-                <span className="font-DanaDemiBold text-base md:text-xl">180,000</span>
+                <span className="font-dana font-semibold text-base md:text-xl">180,000</span>
                 <span className="text-xs md:text-sm tracking-tighter">تومان</span>
               </div>
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
@@ -122,7 +122,7 @@ function Products() {
                 alt="product-3"
               />
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
@@ -132,7 +132,7 @@ function Products() {
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
@@ -165,18 +165,18 @@ function Products() {
                 alt="product-4"
               />
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
               <div className="text-teal-600 dark:text-emerald-500">
-                <span className="font-DanaDemiBold text-base md:text-xl">155,000</span>
+                <span className="font-dana font-semibold text-base md:text-xl">155,000</span>
                 <span className="text-xs md:text-sm tracking-tighter">تومان</span>
               </div>
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
@@ -203,18 +203,18 @@ function Products() {
                 alt="product-5"
               />
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
               <div className="text-teal-600 dark:text-emerald-500">
-                <span className="font-DanaDemiBold text-base md:text-xl">174,000</span>
+                <span className="font-dana font-semibold text-base md:text-xl">174,000</span>
                 <span className="text-xs md:text-sm tracking-tighter">تومان</span>
               </div>
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
@@ -256,18 +256,18 @@ function Products() {
                 alt="product-6"
               />
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
               <div className="text-teal-600 dark:text-emerald-500">
-                <span className="font-DanaDemiBold text-base md:text-xl">158,000</span>
+                <span className="font-dana font-semibold text-base md:text-xl">158,000</span>
                 <span className="text-xs md:text-sm tracking-tighter">تومان</span>
               </div>
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
@@ -296,16 +296,16 @@ function Products() {
                 loading="lazy"
                 alt="product-7"
               />
-              <span className="absolute top-1.5 right-1.5 block h-5 md:h-[30px] text-xs/[24px] md:text-base/[34px] font-DanaDemiBold bg-orange-300 text-white dark:text-zinc-700 px-2.5 md:px-3.5 rounded-full">
+              <span className="absolute top-1.5 right-1.5 block h-5 md:h-7.5 text-xs/[24px] md:text-base/[34px] font-dana font-semibold bg-orange-300 text-white dark:text-zinc-700 px-2.5 md:px-3.5 rounded-full">
                 12%
               </span>
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
               <div className="text-teal-600 dark:text-emerald-500">
-                <span className="font-DanaDemiBold text-base md:text-xl">154,000</span>
+                <span className="font-dana font-semibold text-base md:text-xl">154,000</span>
                 <span className="text-xs md:text-sm tracking-tighter">تومان</span>
               </div>
               <div className="offer">
@@ -315,7 +315,7 @@ function Products() {
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
@@ -348,18 +348,18 @@ function Products() {
                 alt="product-8"
               />
             </div>
-            <h5 className="font-DanaMedium text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
+            <h5 className="font-dana text-sm md:text-xl h-10 md:h-14 text-zinc-700 dark:text-white line-clamp-2">
               قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی
             </h5>
             <div className="flex gap-x-2 md:gap-x-2.5 mt-1.5 md:mt-2.5">
               <div className="text-teal-600 dark:text-emerald-500">
-                <span className="font-DanaDemiBold text-base md:text-xl">154,000</span>
+                <span className="font-dana font-semibold text-base md:text-xl">154,000</span>
                 <span className="text-xs md:text-sm tracking-tighter">تومان</span>
               </div>
             </div>
             <div className="flex items-center justify-between mt-2.5">
               <div className="flex items-center gap-x-2.5 md:gap-x-3">
-                <span className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
+                <span className="flex-center w-6.5 h-6.5 md:w-9 md:h-9 bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 text-gray-400 hover:text-white rounded-full cursor-pointer transition-all">
                   <Icon name="shopping-cart" className="w-4 h-4 md:w-5.5 md:h-5.5" />
                 </span>
                 <span className="block text-gray-400 hover:text-teal-600 dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all">
