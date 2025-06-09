dbf14b96fbac6ffa0288075a63b63c39dc6aad4d
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-09 20:45:03 +0330

    Refactor SectionHeader Component

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -16,23 +16,11 @@ const SectionHeader = (props: SectionHeaderProps): React.JSX.Element => {
         {props.subTitle && <span className="section-subtitle">{props.subTitle}</span>}
       </div>
 
-      {props.swiper ? (
-        <div className="flex gap-x-3 md:gap-x-[18px]">
-          <div className="swiper-button-prev-custom">
-            <Icon name="chevron-left-mini" className="w-5 h-5 md:w-6.5 md:h-6.5 rotate-180" />
-          </div>
-
-          <div className="swiper-button-next-custom">
-            <Icon name="chevron-left-mini" className="w-5 h-5 md:w-6.5 md:h-6.5" />
-          </div>
-        </div>
-      ) : (
-        <a href="#" className="section-link">
-          <span className="hidden md:inline-block">{props.btnTitle}</span>
-          <span className="inline-block md:hidden">مشاهده همه</span>
-          <Icon name="chevron-left-mini" className="w-5 h-5" />
-        </a>
-      )}
+      <a href="#" className="section-link">
+        <span className="hidden md:inline-block">{props.btnTitle}</span>
+        <span className="inline-block md:hidden">مشاهده همه</span>
+        <Icon name="chevron-left-mini" className="w-5 h-5" />
+      </a>
     </div>
   );
 };
