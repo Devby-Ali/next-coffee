9f9d8739071cf0a75c8af6be28c0af0e7d4d5979
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-12 19:03:45 +0330

    Make PruductsCategory fetch in fetchHomePageData Func

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,15 +1,15 @@
 export async function fetchHomePageData() {
   try {
-    const servicesResponse = await fetch('http://localhost:4000/services');
-    const servicesData = await servicesResponse.json();
-
     const productResponse = await fetch('http://localhost:4000/products');
     const productsData = await productResponse.json();
+
+    const productCategoryResponse = await fetch('http://localhost:4000/productCategory');
+    const productCategoryData = await productCategoryResponse.json();
 
     const commentsResponse = await fetch('http://localhost:4000/comments');
     const commentsData = await commentsResponse.json();
 
-    return { servicesData, productsData, commentsData };
+    return { productCategoryData, productsData, commentsData };
   } catch (error) {
     console.error('json-server Error:', error);
     throw new Error('Failed to fetch the fetch Home Page Data.');
