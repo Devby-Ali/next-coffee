c403de5dae2b1a73f68849814540f104be14bd73
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-12 19:05:46 +0330

    Set productsCategory datas For ProductCategory Template

--- src/components/Template/ProductCategory.tsx
+++ src/components/Template/ProductCategory.tsx
@@ -1,51 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
+import { ProductCategoryProps } from '@/types/components.types';
+import ProductCategoryCart from '../common/ProductCategoryCart';
 
-const ProductCategory = (): React.JSX.Element => {
+const ProductCategory = ({ productCategory }: ProductCategoryProps): React.JSX.Element => {
   return (
     <section className="products-category mb-10 md:mb-20">
       <div className="container">
         <div className="flex-center font-semibold gap-y-6 gap-x-8 md:gap-16.5 2xl:gap-x-24 flex-wrap">
-          <div className="w-25 md:w-50 text-center">
-            <a href="#">
-              <Image src="/images/categories/category1.png" height={200} width={200} loading="lazy" alt="c1" />
-            </a>
-            <span className="inline-block text-sm md:text-xl text-zinc-700 dark:text-white mt-1.5 md:mt-2.5">
-              قهوه دمی و اسپرسو
-            </span>
-          </div>
-          <div className="w-25 md:w-50 text-center">
-            <a href="#">
-              <Image src="/images/categories/category2.png" height={200} width={200} loading="lazy" alt="c1" />
-            </a>
-            <span className="inline-block text-sm md:text-xl text-zinc-700 dark:text-white mt-1.5 md:mt-2.5">
-              لوازم جانبی و تجهیزات
-            </span>
-          </div>
-          <div className="w-25 md:w-50 text-center">
-            <a href="#">
-              <Image src="/images/categories/category3.png" height={200} width={200} loading="lazy" alt="c1" />
-            </a>
-            <span className="inline-block text-sm md:text-xl text-zinc-700 dark:text-white mt-1.5 md:mt-2.5">
-              اسپرسو ساز
-            </span>
-          </div>
-          <div className="w-25 md:w-50 text-center">
-            <a href="#">
-              <Image src="/images/categories/category4.png" height={200} width={200} loading="lazy" alt="c1" />
-            </a>
-            <span className="inline-block text-sm md:text-xl text-zinc-700 dark:text-white mt-1.5 md:mt-2.5">
-              پک تستر قهوه
-            </span>
-          </div>
-          <div className="w-25 md:w-50 text-center">
-            <a href="#">
-              <Image src="/images/categories/category5.png" height={200} width={200} loading="lazy" alt="c1" />
-            </a>
-            <span className="inline-block text-sm md:text-xl text-zinc-700 dark:text-white mt-1.5 md:mt-2.5">
-              قهوه ترک
-            </span>
-          </div>
+          {productCategory.map((product) => (
+            <ProductCategoryCart key={product.id} {...product} />
+          ))}
         </div>
       </div>
     </section>
