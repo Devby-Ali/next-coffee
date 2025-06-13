557f6e4f90152b62a6054d45cd00a73f43e8b83f
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-13 14:26:54 +0330

    Make Blogs fetch in fetchHomePageData Func

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,10 +6,14 @@ export async function fetchHomePageData() {
     const productCategoryResponse = await fetch('http://localhost:4000/productCategory');
     const productCategoryData = await productCategoryResponse.json();
 
+
+    const blogsResponse = await fetch('http://localhost:4000/blogs');
+    const blogsData = await blogsResponse.json();
+
     const commentsResponse = await fetch('http://localhost:4000/comments');
     const commentsData = await commentsResponse.json();
 
-    return { productCategoryData, productsData, commentsData };
+    return { productCategoryData, productsData, blogsData, commentsData };
   } catch (error) {
     console.error('json-server Error:', error);
     throw new Error('Failed to fetch the fetch Home Page Data.');
