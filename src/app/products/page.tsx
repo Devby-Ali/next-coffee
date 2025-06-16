b7ea63f212b1b7a94b8dd4d598df76fbbb43bee4
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-16 16:49:32 +0330

    Set LoadMore Component to show more products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,28 +1,46 @@
 import Header from '@/components/common/Header';
-import ProductCart from '@/components/common/ProductCart';
 import SectionHeader from '@/components/ui/SectionHeader';
-import { fetchHomePageData } from '@/lib/data';
-import { ProductsDataProps } from '@/types/components.types';
-import React from 'react';
+import ProductsList from '@/components/ui/products/ProductsList';
 
-async function page() {
-  const data = await fetchHomePageData().then((res) => res);
-  const products = data.productsData;
+async function getInitialProducts() {
+  try {
+    const response = await fetch('http://localhost:4000/products');
+    const products = await response.json();
+
+    const initialProducts = products.slice(0, 10);
+    const totalPages = Math.ceil(products.length / 10);
+
+    return {
+      products: initialProducts,
+      totalPages,
+      currentPage: 1,
+    };
+  } catch (error) {
+    console.error('Error fetching initial products:', error);
+    return {
+      products: [],
+      totalPages: 0,
+      currentPage: 1,
+    };
+  }
+}
+
+export default async function ProductsPage() {
+  const { products, totalPages, currentPage } = await getInitialProducts();
+
   return (
     <>
       <Header />
       <main className="products">
         <div className="container">
           <SectionHeader title="همه محصولات" subTitle="فرآوری شده از دانه قهوه" />
-          <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-3.5 md:gap-5">
-            {products.map((product: ProductsDataProps) => (
-              <ProductCart key={product.id} {...product} />
-            ))}
-          </section>
+          <ProductsList
+            initialProducts={products}
+            initialPage={currentPage}
+            totalPages={totalPages}
+          />
         </div>
       </main>
     </>
   );
 }
-
-export default page;
