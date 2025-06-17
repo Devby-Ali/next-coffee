7ee015d43fb27bc10c3f0366e715aefd705912c0
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-17 23:22:07 +0330

    Make single blogs page

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import BlogDetail from '@/components/blog/BlogDetail';
-import { blogs } from '@/data/db.json';
+import { fetchBlogById } from '@/lib/data';
 
-interface BlogPageProps {
-  params: {
-    id: string;
-  };
-}
 
-const BlogPage = ({ params }: BlogPageProps): React.JSX.Element => {
-  const blog = blogs.find((b) => b.id === parseInt(params.id));
+async function BlogPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const blog = await fetchBlogById(id);
+  // const blog = blogs.find((b) => b.id === parseInt(params.id));
 
   if (!blog) {
     return <div>مقاله مورد نظر یافت نشد.</div>;
