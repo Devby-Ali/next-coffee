f607efd4539f83c0d60b30ca823b17ff637239cc
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-18 22:08:28 +0330

    Fix bug api Address

--- src/components/ui/blogs/BlogsList.tsx
+++ src/components/ui/blogs/BlogsList.tsx
@@ -3,20 +3,16 @@
 import React, { useState } from 'react';
 import BlogCard from './BlogCard';
 import LoadMore from '../LoadMore';
-import { blogProps } from '@/types/components.types';
+import { BlogProps } from '@/types/components.types';
 
 interface BlogsListProps {
-  initialBlogs: blogProps[];
+  initialBlogs: BlogProps[];
   initialPage: number;
   totalPages: number;
 }
 
-const BlogsList: React.FC<BlogsListProps> = ({
-  initialBlogs,
-  initialPage,
-  totalPages,
-}) => {
-  const [blogs, setBlogs] = useState<blogProps[]>(initialBlogs);
+const BlogsList: React.FC<BlogsListProps> = ({ initialBlogs, initialPage, totalPages }) => {
+  const [blogs, setBlogs] = useState<BlogProps[]>(initialBlogs);
   const [currentPage, setCurrentPage] = useState(initialPage);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -25,7 +21,7 @@ const BlogsList: React.FC<BlogsListProps> = ({
 
     try {
       setIsLoading(true);
-      const response = await fetch(`/api/blogs?page=${currentPage + 1}&limit=10`);
+      const response = await fetch(`/api/blogs?page=${currentPage + 1}&limit=8`);
       const data = await response.json();
 
       setBlogs((prev) => [...prev, ...data.blogs]);
@@ -39,9 +35,9 @@ const BlogsList: React.FC<BlogsListProps> = ({
 
   return (
     <>
-      <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-3.5 md:gap-5">
-        {blogs.map((product: blogProps) => (
-          <BlogCard key={product.id} {...product} />
+      <section className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-3.5 md:gap-5">
+        {blogs.map((blog: BlogProps) => (
+          <BlogCard key={blog.id} {...blog} />
         ))}
       </section>
       <LoadMore
