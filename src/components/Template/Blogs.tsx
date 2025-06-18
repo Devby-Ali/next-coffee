3c7e237ba55f45c93e85220b4100898351d3d981
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-18 22:10:26 +0330

    Set responsie grid for blogCard

--- src/components/Template/Blogs.tsx
+++ src/components/Template/Blogs.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import SectionHeader from '../ui/SectionHeader';
-import { blogsProps } from '@/types/components.types';
+import { BlogsProps } from '@/types/components.types';
 import BlogCard from '../ui/blogs/BlogCard';
 
-const Blogs = ({ blogsData }: blogsProps): React.JSX.Element => {
+const Blogs = ({ blogsData }: BlogsProps): React.JSX.Element => {
+  const blogs = blogsData.reverse().slice(0, 8);
   return (
     <section className="blogs mb-8 md:mb-28">
       <div className="container">
@@ -15,7 +16,7 @@ const Blogs = ({ blogsData }: blogsProps): React.JSX.Element => {
         />
         {/* <!-- Section Content --> */}
         <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-3.5 md:gap-5">
-          {blogsData.map((blog) => (
+          {blogs.map((blog) => (
             <BlogCard key={blog.id} {...blog} />
           ))}
         </div>
