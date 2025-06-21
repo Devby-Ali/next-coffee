53bcefda8c15d89243d73b7743a691e70c86fbd7
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-06-21 23:19:18 +0330

    Make fetchProducts Func

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,20 +1,30 @@
 export async function fetchHomePageData() {
   try {
-    const productResponse = await fetch('http://localhost:4000/products');
-    const productsData = await productResponse.json();
-
     const productCategoryResponse = await fetch('http://localhost:4000/productCategory');
     const productCategorydata = await productCategoryResponse.json();
 
     const blogsResponse = await fetch('http://localhost:4000/blogs');
     const blogsData = await blogsResponse.json();
 
-    return { productCategorydata, productsData, blogsData };
+    return { productCategorydata, blogsData };
   } catch (error) {
     console.error('json-server Error:', error);
     throw new Error('Failed to fetch the fetch Home Page Data.');
   }
 }
+
+export async function fetchProducts() {
+  try {
+    const productResponse = await fetch('http://localhost:4000/products');
+    const productsData = await productResponse.json();
+
+    return { productsData };
+  } catch (error) {
+    console.error('json-server Error:', error);
+    throw new Error('Failed to fetch the fetch Products Data.');
+  }
+}
+
 
 export async function fetchProductById(id: string) {
   try {
@@ -27,6 +37,7 @@ export async function fetchProductById(id: string) {
     throw new Error('Failed to fetch product details');
   }
 }
+
 
 export async function fetchBlogById(id: string) {
   try {
